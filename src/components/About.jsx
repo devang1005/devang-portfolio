30f4e5f0145465a47c795a9030a4518e049eb4e3
devang1035 <devangpatel10505@gmail.com>
2025-03-21 20:23:03 +0530

    v12

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import Aboutimg from "../assets/about.jpg";
 import {ABOUT_TEXT} from "../constants";
 import { motion } from "framer-motion";
-import Profilepic from "../assets/devangpic.png";
+import Profilepic from "../assets/Untitled 9.png";
 
 const About = () => {
   return (
